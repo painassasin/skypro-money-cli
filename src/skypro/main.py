7f15cf5ae2a6e6f84cdbd82c774b29ac45fa0ed2
Vadim Meshcheryakov <painassasin@icloud.com>
2026-05-02 03:08:13 +0300

    Move locale settings and logging config to main funciton

--- src/skypro/main.py
+++ src/skypro/main.py
@@ -3,12 +3,10 @@ import locale
 from skypro.cli import app
 from skypro.config.logging import configure_logging
 
-locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
-
-configure_logging()
-
 
 def main() -> None:
+    locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
+    configure_logging()
     app()
 
 
