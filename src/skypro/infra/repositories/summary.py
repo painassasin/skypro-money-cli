7ef39f0f87867ac0f6f96349f92544dcb10f9c7c
Vadim Meshcheryakov <painassasin@icloud.com>
2026-07-07 20:22:23 +0500

    Collect work summary from `services_by_profession`

--- src/skypro/infra/repositories/summary.py
+++ src/skypro/infra/repositories/summary.py
@@ -1,4 +1,7 @@
+import logging
+from collections import defaultdict
 from datetime import date
+from itertools import starmap
 
 from pydantic import ValidationError
 
@@ -6,9 +9,12 @@ from skypro.domain.enums import WorkType
 from skypro.domain.models import WorkSummary
 from skypro.infra.api.errors import ApiError, AuthenticationError
 from skypro.infra.api.skypro.client import SkyProClient
+from skypro.infra.api.skypro.dto import ServiceByProfession, ServiceSummary
 from skypro.infra.http.client import HttpClient
 from skypro.infra.http.errors import HttpError
 from skypro.infra.repositories.errors import SummaryLoadError
+
+logger = logging.getLogger(__name__)
 
 
 async def get_summary(start_date: date, end_date: date) -> list[WorkSummary]:
@@ -22,12 +28,33 @@ async def get_summary(start_date: date, end_date: date) -> list[WorkSummary]:
     except (HttpError, ApiError, ValidationError) as e:
         raise SummaryLoadError(str(e) or 'Failed to load summary from SkyPro.') from e
 
+    if account_data.services_by_profession:
+        return _get_summary_by_service_by_profession(
+            *account_data.services_by_profession
+        )
+
+    return _get_summary_by_service_summary(account_data.services_summary)
+
+
+def _get_summary_by_service_by_profession(
+    *services_by_profession: ServiceByProfession,
+) -> list[WorkSummary]:
+    work_summary: dict[WorkType, int] = defaultdict(int)
+
+    for sbp in services_by_profession:
+        logger.debug('Calculate work summary by %r', sbp.profession)
+        for summary in _get_summary_by_service_summary(sbp.services):
+            work_summary[summary.work_type] += summary.quantity
+    return list(starmap(WorkSummary, work_summary.items()))
+
+
+def _get_summary_by_service_summary(
+    service_summary: ServiceSummary,
+) -> list[WorkSummary]:
     return [
-        WorkSummary(WorkType.HOMEWORK, account_data.services_summary.homework),
-        WorkSummary(WorkType.COURSEWORK, account_data.services_summary.coursework),
-        WorkSummary(WorkType.DIPLOMA, account_data.services_summary.diploma),
-        WorkSummary(WorkType.LIVE, account_data.services_summary.live),
-        WorkSummary(
-            WorkType.CONSULTATION, account_data.services_summary.individual_consultation
-        ),
+        WorkSummary(WorkType.HOMEWORK, service_summary.homework),
+        WorkSummary(WorkType.COURSEWORK, service_summary.coursework),
+        WorkSummary(WorkType.DIPLOMA, service_summary.diploma),
+        WorkSummary(WorkType.LIVE, service_summary.live),
+        WorkSummary(WorkType.CONSULTATION, service_summary.individual_consultation),
     ]
